fc2a332b96ab10c5fa378a26a93325eecbe3a91e
Ignacio Urizar <urizar@MacBook-Air-Urizar.local>
2018-09-20 22:59:21 -0300

    Documents are POST to Raw_Data

--- infoq_feed_script.py
+++ infoq_feed_script.py
@@ -4,9 +4,11 @@ from datetime import datetime
 from urllib.request import urlopen as uReq
 import request
 import feedparser
+import json
+import urllib3
 
 # base url and data from this source
-source_name = "infoQ"
+source_name = "InfoQ"
 source_id = 1
 rss_url= "https://feed.infoq.com/"
 date_format = '%a, %d %b %Y %X %Z'
@@ -22,6 +24,7 @@ entries = d['entries']
 documents = {"documents": [ ]}
 
 # get data from non-recorded entries and original links to publications
+
 for entry in entries:
 
 	# scrap html embeded summary
@@ -37,18 +40,57 @@ for entry in entries:
 
 	# save base data (title, p_date, url, image, source_id, source_name, summary[if-any])
 
-	document['title'] = entry['title']
-	document['url'] = entry['link']
+	title = entry['title']
+	document['title'] = title
+	original_link = entry['link']
+	document['url'] = original_link
 	document['source_id'] = source_id
 	document['source_name'] = source_name
-	document['published'] = datetime.strptime(entry['published'], date_format)
+	datetime_obj = datetime.strptime(entry['published'], date_format)
+	document['published'] = datetime_obj.strftime('%d/%m/%Y')
 	document['main_image'] = summary_soup.img['src']
 	document['summary'] = summary_soup.p.text
 
-	pprint(document)
+	#pprint(document)
 
 	# scrap original link
-	# retrieve & save text from publication
+
+	uClient = uReq(original_link)
+	original_link_content = uClient.read()
+	uClient.close()
+	original_link_soup = BeautifulSoup(original_link_content, 'html.parser')
+
+	# retrieve & save text from publication (p, ul, and blockquote)
+	# InfoQ has 2 kinds of content: publication/articles and presentations
+
+	publication_body = original_link_soup.find("div", {"class": "text_info"})
+	presentation_body = original_link_soup.find("p", {"id": "summary"})
+	raw_text = title
+
+	if publication_body:
+		for p in publication_body.findAll("p", recursive=False):
+			raw_text = " ".join([raw_text, p.text])
+		for ul in publication_body.findAll("ul", recursive=False):
+			raw_text = " ".join([raw_text, ul.text])
+		for bq in publication_body.findAll("blockquote", recursive=False):
+			raw_text = " ".join([raw_text, bq.text])
+
+	if presentation_body:
+		raw_text = " ".join([raw_text, presentation_body.text]) 
+
+	document['raw_text'] = raw_text
+	message = {}
+	message['document'] = document
+
+	# document dict to JSON
+
+	json_message = json.dumps(message)
+
+	# send POST request with json_document to RAW_DATA
+
+	http = urllib3.PoolManager()
+	r = http.request('POST', 'http://raw_data:4000/api/documents', body=json_message, headers={'Content-Type': 'application/json'})
+
 	# add retrieved publication to publication list
 
 # send publication list to RAW_DATA
