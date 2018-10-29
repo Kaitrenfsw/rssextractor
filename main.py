258aaf78b258b1ef8fda33d15c61f0dfc1379525
Ignacio Urizar <urizar@MacBook-Air-Urizar.local>
2018-10-29 19:51:27 -0300

    Master script added

--- main.py
+++ main.py
@@ -1,90 +1,46 @@
-from datetime import datetime
-from threading import Timer
-import feedparser
-import pika
-import re
-import json
+import os
+import time
+
+# create infinite loop to run all scripts
+# leave a 5 minute window between scripts
+# repeat execution after 6 hours
+
+while True:
+
+	print("Starting extraction...")
+
+	# run InfoQ script
+
+	try:
+		print("Running InfoQ feed script...")
+		os.system("python3 infoq_feed_script.py")
+		time.sleep(300)
+
+	except:
+		print("There was a problem running InfoQ script. Skipping...")
 
 
-consulted_pages = ["https://www.reddit.com/r/python/.rss?limit=20"]
+	# run X script
 
-x=datetime.today()
-y=x.replace(day=x.day, hour=x.hour, minute=x.minute+1, second=0, microsecond=0)
-delta_t=y-x
+	# wait and repeat
 
-secs=delta_t.seconds+1
-
-def cleanhtml(raw_html):
-  cleanr = re.compile('<.*?>')
-  cleantext = re.sub(cleanr, '', raw_html)
-  return cleantext
-
-def daily_refreshment():
-	for consulted_page in consulted_pages:
-		today = datetime.today()
-		yesterday = today.replace(day=today.day - 1, hour=1, minute=0, second=0, microsecond=0)
-
-		d = feedparser.parse(consulted_page, modified = yesterday)
-		for post in d.entries:
-			print(post.title)
-
-	x=datetime.today()
-	y=x.replace(day=x.day, hour=x.hour, minute=x.minute+1, second=0, microsecond=0)
-	delta_t=y-x
-	secs=delta_t.seconds+1
-	t = Timer(secs, daily_refreshment)
-	t.start()
-
-
-def get_rss_content(page):
-	pass
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq-docker'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
-
-
-for consulted_page in consulted_pages:
-
-	d = feedparser.parse(consulted_page)
-	message = d.entries[0].title
-	response = {}
-	response["date"] = datetime.today().strftime("%Y-%m-%d")
-	response["documents"] = []
-	documents = []
-	doc_count = 0
-	for post in d.entries:
-		post_response = feedparser.parse(post.url + '/.rss')
-		message = ''
-		
-		post_corpus = {}
-		for post_content in post_response.entries:
-
-			print(cleanhtml( post_content['content'][0]['value'] ))
-			message = message + cleanhtml(post_content['content'][0]['value'])
-
-
-		post_corpus['text'] = message
-		post_corpus['title'] = post.title
-		post_corpus['url'] = post.url
-		post_corpus['site'] = consulted_page
-		post_corpus['site_name'] = consulted_page
-		post_corpus['published'] = datetime.today().strftime("%Y-%m-%d")
-
-		response["documents"].append(post_corpus)
-
-		doc_count += 1
-
-	response["doc_count"] = doc_count
-	print(response)
-	response = json.dumps(response)
-	channel.basic_publish(exchange='',
-	                      routing_key='hello',
-	                      body=response)
-
-import pika
+	print("Next iteration will start in 6 hours...")
+	time.sleep(3600)
+	print("Next iteration will start in 5 hours...")
+	time.sleep(3600)
+	print("Next iteration will start in 4 hours...")
+	time.sleep(3600)
+	print("Next iteration will start in 3 hours...")
+	time.sleep(3600)
+	print("Next iteration will start in 2 hours...")
+	time.sleep(3600)
+	print("Next iteration will start in 1 hours...")
+	time.sleep(1800)
+	print("Next iteration will start in 30 minutes...")
+	time.sleep(900)
+	print("Next iteration will start in 15 minutes...")
+	time.sleep(900)
 
 
 
-t = Timer(secs, daily_refreshment)
-t.start()
+
